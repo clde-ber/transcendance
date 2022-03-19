16565a75f408046896e8082305a488ab41483c58
clde-ber <claire.deberny@gmail.com>
2022-03-19 19:07:43 +0100

    href

--- src/bin/app.js
+++ src/bin/app.js
@@ -2104,6 +2104,12 @@ module.exports = require("./api/router")(window, mountRedraw)
 
 },{"./api/router":2,"./mount-redraw":5}],24:[function(require,module,exports){
 "use strict";
+// import * as mithril from 'mithril';
+// import m from 'mithril';
+// var root = document.body
+// // Try it out! This is just to
+// // help get things started.
+// // EXAMPLE: component composition
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -2132,17 +2138,31 @@ var root = document.body;
 //         ])
 //     }
 // }
+var Subscription = {
+    view() {
+        return (0, mithril_1.default)("h1", { class: "contact" }, "Contact"),
+            (0, mithril_1.default)("div", { class: "logIn" }, [(0, mithril_1.default)("div", { class: "firstName" }, "first name"), (0, mithril_1.default)("div", { class: "lastName" }, "last name"), (0, mithril_1.default)("div", { class: "mobile" }, "mobile"), (0, mithril_1.default)("div", { class: "mail" }, "mail"), (0, mithril_1.default)("div", { class: "address" }, "address"), (0, mithril_1.default)("div", { class: "zipCode" }, "zipcode"), (0, mithril_1.default)("div", { class: "city" }, "city"), (0, mithril_1.default)("div", { class: "country" }, "county")]);
+    }
+};
+var Login = {
+    view() {
+        return (0, mithril_1.default)("h1", { class: "logIn" }, "Log-in"),
+            [(0, mithril_1.default)("div", { class: "username" }, "Username"), (0, mithril_1.default)("div", { class: "password" }, "Password")];
+    }
+};
 var Hello = {
     view() {
         return (0, mithril_1.default)("main", (0, mithril_1.default)("div", { class: "blocks" }, [
             (0, mithril_1.default)("div", { class: "block1" }, [(0, mithril_1.default)("img[src='assets/entry.jpg'][alt='transcendance']", { class: "image" })]),
-            (0, mithril_1.default)("div", { class: "block2" }, [(0, mithril_1.default)("h1", { class: "title" }, "Transcendance"), (0, mithril_1.default)("div", { class: "buttons" }, [(0, mithril_1.default)("button.button[type=button]", { class: "signIn" }, "Sign-in"), (0, mithril_1.default)("button.button[type=button]", { class: "join" }, "Join")])])
+            (0, mithril_1.default)("div", { class: "block2" }, [(0, mithril_1.default)("h1", { class: "title" }, "Transcendance"), (0, mithril_1.default)("div", { class: "buttons" }, [(0, mithril_1.default)(mithril_1.default.route.Link, { href: "/login", class: "signIn" }, "Sign-in"), (0, mithril_1.default)(mithril_1.default.route.Link, { href: "/subscription", class: "join" }, "Join")])])
         ]));
         //m("button.button[type=button]", {class: "signIn"}, "Sign-in"),
         //m("button.button[type=button]", {class: "join"}, "Join"),
     }
 };
 mithril_1.default.mount(root, Hello);
+mithril_1.default.mount(root, Subscription);
+mithril_1.default.mount(root, Login);
 var Splash = {
     view: function () {
         return (0, mithril_1.default)("a", { href: "#!/hello" }, "Enter!");
@@ -2151,6 +2171,11 @@ var Splash = {
 mithril_1.default.route(root, "/splash", {
     "/splash": Splash,
     "/hello": Hello,
+});
+mithril_1.default.route(root, "/hello", {
+    "/hello": Hello,
+    "/subscription": Subscription,
+    "/login": Login,
 });
 /*
 var Hello = {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,74 @@
+// import * as mithril from 'mithril';
+// import m from 'mithril';
+// var root = document.body
+// // Try it out! This is just to
+// // help get things started.
+// // EXAMPLE: component composition
+
+// // class signIn {
+// //     view() {
+// //         return m("button", `Sign-in`)
+// //     }
+// // }
+
+// // class join {
+// //     view() {
+// //         return m("button", `Join`)
+// //     }
+// // }
+
+// // var Hello = {
+// //     view: function() {
+// //         return m("div", [
+// //             m("h1", {"class":"title"}, "Transcendance"),
+// //             m("button", {class: "signIn", type:"button"}, " Sign in "),
+// //             m("button", {class: "join", type:"button"}, " Join ")
+// //         ])
+// //     }
+// // }
+
+// var Subscribe = {
+//     view() {
+//         return m("h1", {class: "contact"}, "Contact"),
+//         m("div", {class: "logIn"}, [m("div", {class: "firstName"}, "first name"), m("div", {class: "lastName"}, "last name"), m("div", {class: "mobile"}, "mobile"), m("div", {class: "mail"}, "mail"), m("div", {class: "address"}, "address"), m("div", {class: "zipCode"}, "zipcode"), m("div", {class: "city"}, "city"), m("div", {class: "country"}, "county")])
+//     }
+// }
+
+// let Hello = {
+//     view() {
+//         return m("main",
+//             m("div", {class: "blocks"}, [
+//             m("div", {class:"block1"}, [m("img[src='assets/entry.jpg'][alt='transcendance']", {class: "image"})]),
+//             m("div", {class:"block2"}, [m("h1", {class: "title"}, "Transcendance"), m("div", {class: "buttons"}, [m("button.button[type=button]", {class: "signIn"}, "Sign-in"), m("button.button[type=button]", {class: "join", onclick: () => { return m("a[href='/Subscribe']", {config: m.route}, "subscribe") }} )])])]))
+//     }
+// }
+
+// let Splash = {
+//     view: function() {
+//         return m("a", {href: "#!/hello"}, "Enter!")
+//     }
+// }
+
+// m.mount(root, Hello)
+// m.mount(root, Subscribe)
+
+// m.route(root, "/splash", {
+//     "/splash": Splash,
+//     "/hello": Hello,
+// })
+
+// /*
+// var Hello = {
+//     view: function() {
+//         return m("main", [
+//             m("h1", {class: "title"}, "Transcendance"),
+//             m("button.sign[type=button]", "Sign-in"),
+//             m("button.button[type=button]", "Join")
+//         ])
+//     }
+// }
+// */
+
 import * as mithril from 'mithril';
 import m from 'mithril';
 var root = document.body
@@ -27,18 +98,34 @@ var root = document.body
 //     }
 // }
 
+var Subscription = {
+    view() {
+            return m("h1", {class: "contact"}, "Contact"),
+            m("div", {class: "logIn"}, [m("div", {class: "firstName"}, "first name"), m("div", {class: "lastName"}, "last name"), m("div", {class: "mobile"}, "mobile"), m("div", {class: "mail"}, "mail"), m("div", {class: "address"}, "address"), m("div", {class: "zipCode"}, "zipcode"), m("div", {class: "city"}, "city"), m("div", {class: "country"}, "county")])
+    }
+}
+
+var Login = {
+    view() {
+        return m("h1", {class: "logIn"}, "Log-in"),
+        [ m("div", {class: "username"}, "Username"), m("div", {class: "password"}, "Password") ]
+    }
+}
+
 var Hello = {
     view() {
         return m("main",
             m("div", {class: "blocks"}, [
             m("div", {class:"block1"}, [m("img[src='assets/entry.jpg'][alt='transcendance']", {class: "image"})]),
-            m("div", {class:"block2"}, [m("h1", {class: "title"}, "Transcendance"), m("div", {class: "buttons"}, [m("button.button[type=button]", {class: "signIn"}, "Sign-in"), m("button.button[type=button]", {class: "join"}, "Join")])])]))
+            m("div", {class:"block2"}, [m("h1", {class: "title"}, "Transcendance"), m("div", {class: "buttons"}, [m(m.route.Link, {href: "/login", class: "signIn"}, "Sign-in"), m(m.route.Link, {href: "/subscription", class: "join"}, "Join")])])]))
             //m("button.button[type=button]", {class: "signIn"}, "Sign-in"),
             //m("button.button[type=button]", {class: "join"}, "Join"),
     }
 }
 
 m.mount(root, Hello)
+m.mount(root, Subscription)
+m.mount(root, Login)
 var Splash = {
     view: function() {
         return m("a", {href: "#!/hello"}, "Enter!")
@@ -47,6 +134,12 @@ var Splash = {
 m.route(root, "/splash", {
     "/splash": Splash,
     "/hello": Hello,
+})
+
+m.route(root, "/hello", {
+    "/hello": Hello,
+    "/subscription": Subscription,
+    "/login": Login,
 })
 
 /*
